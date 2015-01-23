a9816469716a74df1606da2221c0dd25812ab9e5
Charlotte Spencer <charlottelaspencer@gmail.com>
2015-01-23 17:31:56 +0000

    Rename all the item input ids and names. NB Still broken

--- superlists/lists/forms.py
+++ superlists/lists/forms.py
@@ -13,7 +13,8 @@ class ItemForm(forms.models.ModelForm):
             "text": forms.fields.TextInput(attrs={
                 "placeholder": "Enter a to-do item",
                 "class": "form-control input-lg",
-                "id": "item_text",
+                "id": "id_text",
+                "name": "text",
             }),
         }
 
